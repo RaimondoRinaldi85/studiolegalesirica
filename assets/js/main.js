aa8c9d7ff068bb0dc238e63ae8575a21e5d49394
RaimondoRinaldi85 <raimondorinaldi85@gmail.com>
2023-12-16 17:24:45 +0100

    1612/2023 - 17.03

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,29 +254,6 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
 
   /**
    * Testimonials slider
